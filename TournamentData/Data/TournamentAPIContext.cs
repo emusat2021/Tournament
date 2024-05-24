3010f86080cae6b40ceb2dc1c352654d092c6cc6
Eugenia <musat.eugenia@gmail.com>
2024-05-24 11:56:47 +0200

    Add OnModelCreating in DB

--- TournamentData/Data/TournamentAPIContext.cs
+++ TournamentData/Data/TournamentAPIContext.cs
@@ -4,12 +4,17 @@ namespace TournamentData.Data
 {
     public class TournamentAPIContext : DbContext
     {
+
+        public DbSet<TournamentCore.Entities.Tournament> Tournament { get; set; }
+        public DbSet<TournamentCore.Entities.Game> Game { get; set; }
         public TournamentAPIContext (DbContextOptions<TournamentAPIContext> options)
             : base(options)
         {
         }
 
-        public DbSet<TournamentCore.Entities.Tournament> Tournament { get; set; } = default!;
-        public DbSet<TournamentCore.Entities.Game> Game { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
