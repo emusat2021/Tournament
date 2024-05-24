76515b2a737145108cc052430f3436497a9bc712
Eugenia <musat.eugenia@gmail.com>
2024-05-24 11:57:01 +0200

    Refactor Program

--- TournamentAPI/Program.cs
+++ TournamentAPI/Program.cs
@@ -4,20 +4,22 @@ using TournamentAPI.Extensions;
 
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TournamentAPIContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("TournamentAPIContext") ?? throw new InvalidOperationException("Connection string 'TournamentAPIContext' not found.")));
 
 // Add services to the container.
+builder.Services.AddDbContext<TournamentAPIContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("TournamentAPIContext") ?? throw new InvalidOperationException("Connection string 'TournamentAPIContext' not found.")));
 
 builder.Services.AddControllers(opt => opt.ReturnHttpNotAcceptable = true)
     .AddNewtonsoftJson()
     .AddXmlDataContractSerializerFormatters();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Seed the data
 await app.SeedDataAsync();
 
 // Configure the HTTP request pipeline.
