b173a34635df6d9f8012187f9df6b7de093c77d5
Eugenia <musat.eugenia@gmail.com>
2024-05-24 11:58:43 +0200

    Create controller for API Games

--- TournamentAPI/Controllers/GamesController.cs
+++ TournamentAPI/Controllers/GamesController.cs
@@ -15,141 +15,93 @@ namespace TournamentAPI.Controllers
         {
             _context = context;
         }
+
+        // GET: api/Games
         [HttpGet]
-        // GET: Games
-        public async Task<ActionResult<IEnumerable<Game>>> GetGame()
+        public async Task<ActionResult<IEnumerable<Game>>> GetGames()
         {
+            if(_context.Game == null)
+            {
+                return NotFound();
+            }
             return await _context.Game.ToListAsync();
         }
 
-        //// GET: Games/Details/5
-        //[HttpGet("{id}")]
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // GET: api/Games//5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Game>> GetGameById(int? id)
+        {
+            if (_context.Game == null)
+            {
+                return NotFound();
+            }
+            var game = await _context.Game
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
 
-        //    var game = await _context.Game
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (game == null)
-        //    {
-        //        return NotFound();
-        //    }
+            return game;
+        }
 
-        //    return (game);
-        //}
+        // POST: api/game
+        [HttpPost]
+        public async Task<ActionResult<Game>> CreateGame(Game game)
+        {
+                _context.Game.Add(game);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction(nameof(GetGameById), new {id = game.Id}, game);
+        }
 
-        //// GET: Games/Create
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
+        // PUT api/game
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditGame(int id, Game game)
+        {
+            if(id !=game.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(game).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if(!GameExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
 
-        //// POST: Games/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("Id,Title,Time,TournamentId")] Game game)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(game);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(game);
-        //}
+        // Delete: api/game
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Game>> DeleteGame(int id)
+        {
+            if (_context.Game == null)
+            {
+                return NotFound();
+            }
+            var game = await _context.Game.FindAsync(id);
+            if(game == null)
+            {
+                return NotFound();
+            }
+            _context.Game.Remove(game);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
 
-        //// GET: Games/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var game = await _context.Game.FindAsync(id);
-        //    if (game == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(game);
-        //}
-
-        //// POST: Games/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Time,TournamentId")] Game game)
-        //{
-        //    if (id != game.Id)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(game);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!GameExists(game.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(game);
-        //}
-
-        //// GET: Games/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var game = await _context.Game
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (game == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(game);
-        //}
-
-        //// POST: Games/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var game = await _context.Game.FindAsync(id);
-        //    if (game != null)
-        //    {
-        //        _context.Game.Remove(game);
-        //    }
-
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
-
-        //private bool GameExists(int id)
-        //{
-        //    return _context.Game.Any(e => e.Id == id);
-        //}
+        private bool GameExists(int id)
+        {
+            return _context.Game.Any(e => e.Id == id);
+        }
     }
 }
