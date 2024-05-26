23fc7136eae41dcc456335081fb2822dc0837e12
Eugenia <musat.eugenia@gmail.com>
2024-05-26 12:12:39 +0200

    Add configuration

--- TournamentData/Data/TournamentAPIContext.cs
+++ TournamentData/Data/TournamentAPIContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TournamentCore.Entities;
 
 namespace TournamentData.Data
 {
@@ -7,7 +8,7 @@ namespace TournamentData.Data
 
         public DbSet<TournamentCore.Entities.Tournament> Tournament { get; set; }
         public DbSet<TournamentCore.Entities.Game> Game { get; set; }
-        public TournamentAPIContext (DbContextOptions<TournamentAPIContext> options)
+        public TournamentAPIContext(DbContextOptions<TournamentAPIContext> options)
             : base(options)
         {
         }
@@ -15,6 +16,33 @@ namespace TournamentData.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Tournament>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title)
+                      .IsRequired()
+                      .HasMaxLength(100);
+                entity.Property(e => e.StartDate)
+                      .IsRequired();
+
+                // Configure one-to-many relationship
+                entity.HasMany(e => e.Games)
+                      .WithOne(g => g.Tournament)
+                      .HasForeignKey(g => g.TournamentId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            // Configure Game entity
+            modelBuilder.Entity<Game>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title)
+                      .IsRequired()
+                      .HasMaxLength(100);
+                entity.Property(e => e.Time)
+                      .IsRequired();
+            });
         }
     }
 }
